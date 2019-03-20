482f35a27cede0cd79533f8a895a0ede7cc2afa8
Nazar Sytnyk <sutnuk23nazar@gmail.com>
2019-03-20 16:11:44 +0200

    Edit Task7

--- Task7.js
+++ Task7.js
@@ -1,23 +1,21 @@
 var arr = [1,2,3,4,5,6,7]
 function Numbers(arr){
 var newArr =[];
-var func;
 for(var val of arr){
-	func = Odd(val);
 	var obj = {};
 	obj.value = val;
-	obj.method = func;
+	obj.method = function()
+	{
+		return this.value % 2 == 0;
+	};
 	newArr.push(obj); 
 }
 return newArr;
 }
-
-function Odd(val){
-	if(val % 2 == 0)
-		return false;
-	else 
-		return true;
-}
  var res = Numbers(arr);
- for(var val of res)
-	 alert(val.value + "-" + val.method);
+
+
+for(var val of res)
+	 console.log(val.value + "-" + val.method());
+
+ 
